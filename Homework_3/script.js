568a3a39691894eec477a0a0cc4f35203466eae0
Viacheslav137 <slavazaslavskiy37@gmail.com>
2024-11-26 00:13:26 +0100

    Add files via upload
    Initial commit

---
+++ Homework_3/script.js
@@ -0,0 +1,22 @@
+// Task_1
+
+// const number = +prompt("Введите число")
+// if(number === 0) {
+//     console.log("Число равно нулю")
+// } else if(number % 2 === 0) 
+// {
+//     console.log("Число четное")
+// }
+// else if(number % 2 !== 0 ) {
+//     console.log("Число не четное")
+// }
+
+
+// Task_2
+
+// var answer = +prompt("ВВедите число" , "Ваше значение")
+// if (isNaN(answer)) {
+//     console.log("Это значение переоббразовать в число не получится NaN")
+    // }else {
+//     console.log("Это значение можно преобразовать в число")
+// }
